61243999dfe804920fa83f10c150c3c0255986bf
git-kth <hisidblack@kakao.com>
2023-05-02 15:56:57 +0900

    Member entity 생성

---
+++ cslab/src/main/kotlin/kr/co/devcs/cslab/entity/Member.kt
@@ -0,0 +1,37 @@
+package kr.co.devcs.cslab.entity
+
+import jakarta.persistence.*
+import org.hibernate.annotations.CreationTimestamp
+import java.time.LocalDateTime
+
+@Entity
+class Member (
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        val id: Long? = null,
+
+        @Column(unique = true)
+        val email: String,
+
+        val password: String,
+
+        @Column(unique = true)
+        val sno: String,
+
+        val name: String,
+
+        val birthDate: LocalDateTime,
+
+        @Enumerated(EnumType.STRING)
+        @ElementCollection(fetch = FetchType.EAGER)
+        val roles: MutableSet<MemberRole>,
+
+        @CreationTimestamp
+        val createdDate: LocalDateTime = LocalDateTime.now(),
+
+        @Column(nullable = false)
+        val isAdmin: Boolean = false,
+
+        @Column(nullable = false)
+        val isEnabled: Boolean = false
+)
