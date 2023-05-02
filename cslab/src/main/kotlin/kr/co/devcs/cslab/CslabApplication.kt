b31a0e64bab6f5ceb630cb87b5f478ba5e5b56f7
git-kth <hisidblack@kakao.com>
2023-05-02 15:28:15 +0900

    spring initializr code generate

---
+++ cslab/src/main/kotlin/kr/co/devcs/cslab/CslabApplication.kt
@@ -0,0 +1,11 @@
+package kr.co.devcs.cslab
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CslabApplication
+
+fun main(args: Array<String>) {
+	runApplication<CslabApplication>(*args)
+}
---
+++ cslab/src/test/kotlin/kr/co/devcs/cslab/CslabApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.co.devcs.cslab
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CslabApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
